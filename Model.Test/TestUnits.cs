672ca549a22166bc945ed96980f60236af66c700
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-11-17 02:47:55 +0300

    Добавлены классы единиц измерения и юнит тесты к ним

---
+++ Model.Test/TestUnits.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.Recipe;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class TestGramm
+    {
+        [Test]
+        public void TestName()
+        {
+            var unit = new Gramm();
+            Assert.AreEqual("г.", unit.Name);
+        }
+
+        [Test]
+        public void TestNegativeGain()
+        {
+            var unit = new Gramm();
+            Assert.Throws<ArgumentException>(() => unit.ChangeAmount(10, -1));
+        }
+
+        [Test]
+        public void TestReduceNoRound()
+        {
+            var unit = new Gramm();
+            Assert.AreEqual(5, unit.ChangeAmount(10, 0.5));
+        }
+
+        [Test]
+        public void TestReduceRound()
+        {
+            var unit = new Gramm();
+            Assert.AreEqual(2, unit.ChangeAmount(5, 0.5));
+        }
+
+        [Test]
+        public void TestIncreaseRound()
+        {
+            var unit = new Gramm();
+            Assert.AreEqual(20, unit.ChangeAmount(10, 2));
+        }
+    }
+
+    public class TestMilliliter
+    {
+        [Test]
+        public void TestName()
+        {
+            var unit = new Milliliter();
+            Assert.AreEqual("мл.", unit.Name);
+        }
+
+        [Test]
+        public void TestNegativeGain()
+        {
+            var unit = new Milliliter();
+            Assert.Throws<ArgumentException>(() => unit.ChangeAmount(10, -1));
+        }
+
+        [Test]
+        public void TestReduceNoRound()
+        {
+            var unit = new Milliliter();
+            Assert.AreEqual(5, unit.ChangeAmount(10, 0.5));
+        }
+
+        [Test]
+        public void TestReduceRound()
+        {
+            var unit = new Milliliter();
+            Assert.AreEqual(2, unit.ChangeAmount(5, 0.5));
+        }
+
+        [Test]
+        public void TestIncreaseRound()
+        {
+            var unit = new Milliliter();
+            Assert.AreEqual(20, unit.ChangeAmount(10, 2));
+        }
+    }
+
+    public class TestPiece
+    {
+        [Test]
+        public void TestName()
+        {
+            var unit = new Piece();
+            Assert.AreEqual("шт.", unit.Name);
+        }
+
+        [Test]
+        public void TestNegativeGain()
+        {
+            var unit = new Piece();
+            Assert.Throws<ArgumentException>(() => unit.ChangeAmount(10, -1));
+        }
+
+        [Test]
+        public void TestReduceNoRound()
+        {
+            var unit = new Piece();
+            Assert.AreEqual(5, unit.ChangeAmount(10, 0.5));
+        }
+
+        [Test]
+        public void TestReduceRound()
+        {
+            var unit = new Piece();
+            Assert.AreEqual(3, unit.ChangeAmount(5, 0.5));
+        }
+
+        [Test]
+        public void TestIncreaseRound()
+        {
+            var unit = new Piece();
+            Assert.AreEqual(20, unit.ChangeAmount(10, 2));
+        }
+    }
+}
---
+++ Model/Recipe/Units.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Recipe
+{
+    /// <summary>
+    /// Базовый класс единицы измерения количества ингредиентов.
+    /// Этот класс описывает количество ингредиента. Он используется, потому
+    /// что количество ингредиента определяется не только численным параметром,
+    /// но и единицей измерения (граммы, штуки и т.п.)
+    /// </summary>
+    public abstract class BaseUnit
+    {
+        /// <summary>
+        /// Название единицы измерения
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Пересчитывает количество игредиента при изменении объема готового блюда
+        /// </summary>
+        /// <param name="amount">Исходное количество</param>
+        /// <param name="gain">Коэффициент изменения (0; +inf)</param>
+        /// <returns></returns>
+        public abstract int ChangeAmount(uint amount, double gain);
+    }
+
+    /// <summary>
+    /// Граммы
+    /// </summary>
+    public class Gramm : BaseUnit
+    {
+        public override string Name => "г.";
+
+        public override int ChangeAmount(uint amount, double gain)
+        {
+            if (gain <= 0)
+            {
+                throw new ArgumentException();
+            }
+            return (int)(amount * gain);
+        }
+    }
+
+    /// <summary>
+    /// Миллилитр
+    /// </summary>
+    public class Milliliter : BaseUnit
+    {
+        public override string Name => "мл.";
+
+        public override int ChangeAmount(uint amount, double gain)
+        {
+            if (gain <= 0)
+            {
+                throw new ArgumentException();
+            }
+            return (int)(amount * gain);
+        }
+    }
+
+    /// <summary>
+    /// Штука
+    /// </summary>
+    public class Piece : BaseUnit
+    {
+        public override string Name => "шт.";
+
+        public override int ChangeAmount(uint amount, double gain)
+        {
+            if (gain <= 0)
+            {
+                throw new ArgumentException();
+            }
+            return (int)Math.Ceiling(amount * gain);
+        }
+    }
+}
