3772b9afe1f53a27a21c20fbad3946f4d334079a
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-11-17 03:07:01 +0300

    Добавлен класс Recipe

---
+++ Model/Recipe/Ingredient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Recipe
+{
+    /// <summary>
+    /// Класс ингредиента
+    /// </summary>
+    public class Ingredient
+    {
+        public string Name { get; }
+        public BaseUnit Unit { get;}
+
+        public Ingredient(string name, BaseUnit unit)
+        {
+            Name = name;
+            Unit = unit;
+        }
+    }
+
+    /// <summary>
+    /// Ингредиент со списком замен
+    /// </summary>
+    public class ListIngredient : Ingredient
+    {
+        private List<Ingredient> alternatives;
+
+        public ListIngredient(string name, BaseUnit unit) : base(name, unit)
+        {
+            // TODO: надо как-то заполнять этот список, когда создаем из БД
+            alternatives = new List<Ingredient>();
+        }
+
+        /// <summary>
+        /// Получить список замен
+        /// </summary>
+        /// <returns></returns>
+        public List<Ingredient> GetAlternatives()
+        {
+            return alternatives;
+        }
+    }
+}
---
+++ Model/Recipe/Recipe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Recipe
+{
+    public abstract class Recipe
+    {
+        public string Name { get; set; }
+        public string RecipeText { get; set; }
+        public List<RecipeItem> Ingredients { get; set; }
+        public Amount Amount { get; set; }
+
+        public abstract void SetAmount(Amount amount);
+        public List<RecipeItem> GetIngredients()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RecipeItem
+    {
+        public Ingredient Ingredient { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class Amount
+    {
+        public BaseUnit Unit { get; set; }
+        public int Value { get; set; }
+    }
+
+}
