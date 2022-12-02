52e646457f9a7b514aca9f92e06f5170d42e52a7
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-12-03 01:45:35 +0300

    Добавление фейкового источника данных
     - Добавлен проект для работы с данными
     - Создан интерфейс источника данных
     - Создана фейковая реализация интерфейса, которая возвращает пример данных

---
+++ Data/FakeData.cs
@@ -0,0 +1,68 @@
+﻿using Model.Recipe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    /// <summary>
+    /// Фейковая реализация интерфейса хранилища для тестирования и отладки
+    /// </summary>
+    public class FakeData : IDataSource
+    {
+        private List<BaseRecipe> recipes;
+        BaseUnit gr, ml, pcs;
+
+        public FakeData()
+        { 
+            recipes = new List<BaseRecipe>();
+
+            gr = new Gramm();
+            ml = new Milliliter();
+            pcs = new Piece();
+
+            var water = new Ingredient("Вода", ml);
+            var milk = new Ingredient("Молоко", ml);
+            var flover = new Ingredient("Мука", gr);
+            var potato = new Ingredient("Картофель", gr);
+            var egg = new Ingredient("Яйцо куриное", pcs);
+            var onion = new Ingredient("Лук", pcs);
+            var chicken = new Ingredient("Филе куриное", gr);
+            var tomato = new Ingredient("Помидор", pcs);
+            var beans = new Ingredient("Фасоль", gr);
+
+            recipes.Add(new SimpleRecipe(
+                "Драники",
+                "Рецепт",
+                new List<RecipeItem>
+                {
+                    new RecipeItem(potato, 800),
+                    new RecipeItem(flover, 50),
+                    new RecipeItem(egg, 1),
+                    new RecipeItem(onion, 1),
+                },
+                new Amount(pcs, 6)
+            ));
+
+            recipes.Add(new SimpleRecipe(
+                "Корм",
+                "Рецепт",
+                new List<RecipeItem>
+                {
+                    new RecipeItem(chicken, 800),
+                    new RecipeItem(tomato, 4),
+                    new RecipeItem(onion, 1),
+                    new RecipeItem(beans, 300),
+                },
+                new Amount(pcs, 8)
+            ));
+        }
+
+        public IReadOnlyList<BaseRecipe> GetRecipes()
+        {
+            return recipes;
+        }
+    }
+}
---
+++ Data/IDataSource.cs
@@ -0,0 +1,21 @@
+﻿using Model.Recipe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    /// <summary>
+    /// Интерфейс источника данных
+    /// </summary>
+    public interface IDataSource
+    {
+        /// <summary>
+        /// Получить список рецептов
+        /// </summary>
+        /// <returns>Список рецептов</returns>
+        IReadOnlyList<BaseRecipe> GetRecipes();
+    }
+}
