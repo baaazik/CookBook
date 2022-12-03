ed277b64d38f10e71c842c96c0636af22e05bfd1
Zabaznova Anzhelika <zabaznova2303@gmail.com>
2022-12-03 06:08:24 +0300

    Реализовано составление списка покупок

---
+++ GUI/ShoppingList.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GUI
+{
+    partial class ShoppingList
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.listBoxShopping = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(4, 4);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(120, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Обновить";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // listBoxShopping
+            // 
+            this.listBoxShopping.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxShopping.FormattingEnabled = true;
+            this.listBoxShopping.ItemHeight = 15;
+            this.listBoxShopping.Location = new System.Drawing.Point(4, 33);
+            this.listBoxShopping.Name = "listBoxShopping";
+            this.listBoxShopping.Size = new System.Drawing.Size(372, 214);
+            this.listBoxShopping.TabIndex = 4;
+            // 
+            // ShoppingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listBoxShopping);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "ShoppingList";
+            this.Size = new System.Drawing.Size(380, 260);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ListBox listBoxShopping;
+    }
+}
